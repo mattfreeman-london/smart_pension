d132bd0a22b7842034eb0a3980daece04f9cccef
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-02-19 12:15:14 +0000

    refactor: tidied all tests

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -4,28 +4,24 @@ require 'page'
 
 describe Page do
   describe '#count' do
+    let (:page) { Page.new }
     it 'counts a single page view' do
-      page = Page.new
       expect(page.count('/home')).to eq('/home - visits 1')
     end
 
     it 'counts different page names' do
-      page = Page.new
       expect(page.count('/about')).to eq('/about - visits 1')
     end
 
     it 'can count any number of views of one page' do
-      page = Page.new
       expect(page.count('/home,/home,/home')).to eq('/home - visits 3')
     end
 
     it 'can count one instance of two different pages' do
-      page = Page.new
       expect(page.count('/home,/about')).to eq('/home - visits 1, /about - visits 1')
     end
 
     it 'can count multiple instances of two different pages' do
-      page = Page.new
       expect(page.count('/home,/about,/home,/about')).to eq('/home - visits 2, /about - visits 2')
     end
   end
