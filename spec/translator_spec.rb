50d3ec8ca639f3624da42be835d9346c921fe93b
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-02-20 14:40:57 +0000

    refacto: changed get_file_as_string test to be more useful

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,8 +1,8 @@
 require 'translator'
 
 describe '#get_file_as_string' do
-  it 'turns the log file into a string' do
-    expect(get_file_as_string('webserver.log')).to be_a String
+  it 'turns a log file into a string' do
+    expect(get_file_as_string('test_file.log')).to eq("/home_page\n/about\n")
   end
 end
 
