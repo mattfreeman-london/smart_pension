6266e12541b3e844582924e8db942b1c62a54b0d
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-02-20 15:10:03 +0000

    update: refactored tests to be more like actual input

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -6,13 +6,13 @@ describe Translator do
   let(:translator) { Translator.new }
   describe '#get_file_as_string' do
     it 'turns a log file into a string' do
-      expect(translator.get_file_as_string('test_file.log')).to eq("/home_page\n/about\n")
+      expect(translator.get_file_as_string('test_file.log')).to eq("/home_page 814\n/about 345\n")
     end
   end
 
   describe '#string_to_array' do
     it 'creates an array from the string' do
-      expect(translator.string_to_array('/home_page\n/about\n')).to eq(['/home_page', '/about'])
+      expect(translator.string_to_array('/home_page 814\n/about 345\n')).to eq(['/home_page 814', '/about 345'])
     end
   end
 end
