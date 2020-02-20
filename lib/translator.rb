e6d9d8939df574ae2d6643330bd01ea5b22e947a
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-02-20 14:54:27 +0000

    refactor: tidied for rubocop

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,16 +1,18 @@
-class Translator
+# frozen_string_literal: true
 
+# to turn a log file into a useful format
+class Translator
   def get_file_as_string(filename)
     data = ''
-    file = File.open(filename, "r")
+    file = File.open(filename, 'r')
     file.each_line do |line|
       data += line
     end
-    return data
+    data
   end
 
   def string_to_array(string)
     array = string.split('\n')
-    return array
+    array
   end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'translator'
 
 describe Translator do
-  let (:translator) { Translator.new }
-    describe '#get_file_as_string' do
-      it 'turns a log file into a string' do
-        expect(translator.get_file_as_string('test_file.log')).to eq("/home_page\n/about\n")
-      end
+  let(:translator) { Translator.new }
+  describe '#get_file_as_string' do
+    it 'turns a log file into a string' do
+      expect(translator.get_file_as_string('test_file.log')).to eq("/home_page\n/about\n")
     end
+  end
 
-    describe '#string_to_array' do
-      it 'creates an array from the string' do
-        expect(translator.string_to_array('/home_page\n/about\n')).to eq(["/home_page", "/about"])
-      end
+  describe '#string_to_array' do
+    it 'creates an array from the string' do
+      expect(translator.string_to_array('/home_page\n/about\n')).to eq(['/home_page', '/about'])
     end
+  end
 end
